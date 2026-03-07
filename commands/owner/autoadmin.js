ac32b6bb086f3bf0353d13c79e4a3f6417799fc7
Jarednnnn <patylachina3@gmail.com>
2026-03-06 21:33:23 -0500

    Update autoadmin.js

--- commands/owner/autoadmin.js
+++ commands/owner/autoadmin.js
@@ -1,8 +1,20 @@
 export default {
-  command: ['auoadmin'],
+  command: ['autoadmin'],
+  category: 'grupo',
   isOwner: true,
+  botAdmin: true,
   run: async (client, m, args, usedPrefix, command) => {
     const sender = m.sender
-    await client.sendMessage(m.chat, { text: `A sus órdenes, @${sender.split('@')[0]}`, mentions: [sender] }, { quoted: m })
+    try {
+      const groupMetadata = await client.groupMetadata(m.chat)
+      const participant = groupMetadata.participants.find(p => p.id === sender)
+      if (participant?.admin) {
+        return client.sendMessage(m.chat, { text: `Usted ya tiene admin, mi señor.`, mentions: [sender] }, { quoted: m })
+      }
+      await client.groupParticipantsUpdate(m.chat, [sender], 'promote')
+      await client.sendMessage(m.chat, { text: `A sus órdenes, @${sender.split('@')[0]}`, mentions: [sender] }, { quoted: m })
+    } catch (e) {
+      await m.reply(`> Ocurrió un error al ejecutar el comando *${usedPrefix + command}*. Por favor, intente de nuevo o contacte a soporte si el problema persiste.\n> [Error: *${e.message}*]`)
+    }
   }
 }
